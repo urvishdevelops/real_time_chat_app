1e16549c25a4fd75e5705c81573733a79e9b4ca3
Urvish Jaiswal <135489625+urvishdevelops@users.noreply.github.com>
2024-03-25 11:45:06 +0530

    socket.io integrated

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
-const ws = require("ws");
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-const server = new ws.Server({ port: "3000" });
+const httpServer = createServer();
 
-server.on("connection", (socket) => {
-  socket.on("message", (message) => {
-    const b = Buffer.from(message);
-    console.log(b.toString());
-    socket.send(`${message}`);
+// server initialized and converting http into socket
+const io = new Server(httpServer, {
+  cors: {
+    origin:
+      process.env.Node_ENV === "production"
+        ? false
+        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id.substring(0,5)} connected`);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message", `${socket.id.substring(0, 5)} : ${data}`);
   });
+
+
+});
+
+// listening http server
+httpServer.listen(3500, () => {
+  console.log("listening on the port 3500");
 });
